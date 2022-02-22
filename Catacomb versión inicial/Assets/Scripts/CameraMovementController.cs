b20c702676080aa1b2c7dae0e6fffb47d4f9aa2b
frmolla <frmolla@ucm.es>
2022-02-22 22:57:00 +0100

    CameraMovementController

---
+++ Catacomb versión inicial/Assets/Scripts/CameraMovementController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    float smoothSpeed = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 desiredPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+        transform.position = smoothedPos;
+    }
+}
