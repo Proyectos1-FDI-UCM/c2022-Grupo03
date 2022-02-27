85b8eb9bce22fb0060d0e2d69af665d1d862b36f
pedroleo <pedroleo@ucm.es>
2022-02-27 01:43:41 +0100

    Flecha de dir independiente del personaje

--- Catacomb versión inicial/Assets/Scripts/Jugador/DirectionArrow.cs
+++ Catacomb versión inicial/Assets/Scripts/Jugador/DirectionArrow.cs
@@ -13,8 +13,11 @@ public class DirectionArrow : MonoBehaviour
     #endregion
 
     #region references
-    Transform _myTransform;
+    [SerializeField]
+    GameObject _player;
+    Transform _playerTransform;
     Camera _mainCamera;
+    Transform _myTransform;
     #endregion
 
     #region methods
@@ -30,15 +33,20 @@ public class DirectionArrow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _myTransform = transform;
+        _playerTransform = _player.transform;
         _mainCamera = Camera.main;
+        _myTransform = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // hacer que la flecha de dirección esté siempre en la posición del jugador
+        _myTransform.position = _playerTransform.position;
+
+        // dirección de la flecha, que indica hacia donde se está apuntando
         Vector3 targetPoint = WorldPointWithoutZ(Input.mousePosition);
-        Vector3 dir = targetPoint - _myTransform.position;
+        Vector3 dir = targetPoint - _playerTransform.position;
         _myTransform.right = dir.normalized;
     }
 }
