06e0cf7bf5a604319a4e5a0d6f14fc66fe3a071e
Jjianuo <Jwen@ucm.es>
2022-03-01 22:53:12 +0100

    Añadido prefabs de muros y destructor de balas

--- Catacomb versión inicial/Assets/Scripts/Enemigos/ProjectileMovement.cs
+++ Catacomb versión inicial/Assets/Scripts/Enemigos/ProjectileMovement.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class ProjectileMovement : MonoBehaviour
 {
+    #region parameters
     [SerializeField]
     private float _projectileSpeed = 5.0f;
+    private float lifespan = 8.0f;
+    private float _elapsedTime = 0;
+    #endregion
+    #region references
     private GameObject target;
     private Transform _myTransform;
     Rigidbody2D hitbox;
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +26,20 @@ public class ProjectileMovement : MonoBehaviour
             _myTransform.up = temp;
             hitbox = GetComponent<Rigidbody2D>();
             hitbox.velocity = (_myTransform.up * _projectileSpeed);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+        if (_elapsedTime > lifespan)
+        {
+            Destroy(this.gameObject);
         }
     }
 }
