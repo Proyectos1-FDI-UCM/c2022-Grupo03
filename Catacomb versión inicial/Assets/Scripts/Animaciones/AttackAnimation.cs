da9e1615b573dd47f6486ac86534f741560b6a57
marsache <marsache@ucm.es>
2022-03-03 17:59:00 +0100

    animación ataque azul (por terminar)

--- Catacomb versión inicial/Assets/Scripts/Animaciones/AttackAnimation.cs
+++ Catacomb versión inicial/Assets/Scripts/Animaciones/AttackAnimation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RollAnimation : MonoBehaviour
+public class AttackAnimation : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
