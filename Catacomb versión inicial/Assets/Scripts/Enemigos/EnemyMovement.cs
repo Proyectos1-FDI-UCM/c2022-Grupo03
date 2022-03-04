dcdc18a65f6000ad3f887e4e1c7f35e0a0691242
Jjianuo <Jwen@ucm.es>
2022-03-04 22:10:31 +0100

    IA de los enemigos

--- Catacomb versión inicial/Assets/Scripts/Enemigos/EnemyMovement.cs
+++ Catacomb versión inicial/Assets/Scripts/Enemigos/EnemyMovement.cs
@@ -11,10 +11,12 @@ public class EnemyMovement : MonoBehaviour
     private float _range = 5.0f;
     [SerializeField]
     private bool ranged = false;
+    private int layers;
     #endregion
 
     #region properties
     protected Vector3 _movementDirection;
+    protected Vector3 _playerDirection;
     #endregion
 
     #region references
@@ -22,12 +24,17 @@ public class EnemyMovement : MonoBehaviour
     private bool _isReloading;
     private GameObject targetObject;
     private Transform targetTransform;
+    private RaycastHit2D hit;
+    private RaycastHit2D leftRay;
+    private RaycastHit2D rightRay;
+    private int offset;
+
     #endregion
 
     #region methods
-    private void SetMovementDirection()
+    private void SetPlayerDirection()
     {
-        _movementDirection = (targetTransform.position - this.transform.position).normalized;
+        _playerDirection = (targetTransform.position - this.transform.position).normalized;
     }
 
     private void SetEscapeDirection()
@@ -42,24 +49,51 @@ public class EnemyMovement : MonoBehaviour
         _myTransform = transform;
         targetObject = GameObject.Find("Player");
         targetTransform = targetObject.transform;
-        SetMovementDirection();
+        SetPlayerDirection();
+        offset = 45;
+        RaycastHit2D hitinfo  = Physics2D.Raycast(this.transform.position, _movementDirection);
+        layers = 1 << 8 | 1 << 3;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //SetMovementDirection();
-        Vector3 temp = (targetTransform.position - this.transform.position);
-        if(ranged) _isReloading = GetComponentInChildren<EnemyShooter>().reloading;
-        if (temp.x > _range || temp.y > _range)
+        Vector3 temp = (targetTransform.position - transform.position);
+        Debug.DrawRay(transform.position, _playerDirection *  100.0f, Color.red, 1.0f);
+        hit = Physics2D.Raycast(transform.position, temp.normalized, 100.0f, layers);
+        SetPlayerDirection();
+        if (hit.collider == targetObject.GetComponent<Collider2D>())
         {
-            SetMovementDirection();
-            _myTransform.Translate(_speed * _movementDirection * Time.deltaTime);
+            _movementDirection = temp.normalized;
+            if (ranged) _isReloading = GetComponentInChildren<EnemyShooter>().reloading;
+            if (hit.distance > _range) SetPlayerDirection();
+            else if (_isReloading && (hit.distance < _range)) SetEscapeDirection();
         }
-        else if (_isReloading && (temp.x < _range / 2 || temp.y < _range / 2))
+        else
         {
-            SetEscapeDirection();
-            _myTransform.Translate(_speed * _movementDirection * Time.deltaTime);
+            Vector3 left = Quaternion.Euler(0, 0, offset) * _playerDirection;
+            Vector3 right = Quaternion.Euler(0, 0, -offset) * _playerDirection;
+            Debug.DrawRay(transform.position, left.normalized * 100.0f, Color.green, 1.0f);
+            Debug.DrawRay(transform.position, right.normalized * 100.0f, Color.yellow, 1.0f);
+            leftRay = Physics2D.Raycast(this.transform.position, left.normalized, 100.0f);
+            rightRay = Physics2D.Raycast(this.transform.position, right.normalized, 100.0f);
+            if (leftRay.distance > rightRay.distance) _movementDirection = left.normalized;
+            else if (rightRay.distance > leftRay.distance) _movementDirection = right.normalized;
+            else if (leftRay.distance == 0) _movementDirection = left.normalized;
+            else if (rightRay.distance == 0) _movementDirection = right.normalized;
+            Debug.Log("Distancia l: " + leftRay.distance);
+            Debug.Log("Distancia r: " + rightRay.distance);
         }
+        _myTransform.Translate(_speed * _movementDirection * Time.deltaTime);
     }
+
+
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    Vector3 direc = new Vector3(0, 0, -90);
+    //    _movementDirection += direc;
+    //    Debug.Log("moving");
+    //}
 }
--- Catacomb versión inicial/Assets/Scripts/Enemigos/ProjectileMovement.cs
+++ Catacomb versión inicial/Assets/Scripts/Enemigos/ProjectileMovement.cs
@@ -9,11 +9,24 @@ public class ProjectileMovement : MonoBehaviour
     private float _projectileSpeed = 5.0f;
     private float lifespan = 8.0f;
     private float _elapsedTime = 0;
+    private int damage = 1;
     #endregion
     #region references
     private GameObject target;
     private Transform _myTransform;
     Rigidbody2D hitbox;
+    #endregion
+
+    #region methods
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerLifeComponent _playerLifeComponent = collision.gameObject.GetComponent<PlayerLifeComponent>();
+        if (_playerLifeComponent != null)
+        {
+            _playerLifeComponent.Damage(damage);
+        }
+        Destroy(this.gameObject);
+    }
     #endregion
     // Start is called before the first frame update
     void Start()
@@ -28,12 +41,6 @@ public class ProjectileMovement : MonoBehaviour
             hitbox.velocity = (_myTransform.up * _projectileSpeed);
         }
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Destroy(this.gameObject);
-    }
-
     private void Update()
     {
         _elapsedTime += Time.deltaTime;
