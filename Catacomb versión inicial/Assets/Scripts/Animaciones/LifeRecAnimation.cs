68bec32b0f50424e96e3f4065614744301deea22
marsache <marsache@ucm.es>
2022-04-01 12:23:37 +0200

    animación de vida en proceso

---
+++ Catacomb versión inicial/Assets/Scripts/Animaciones/LifeRecAnimation.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeRecAnimation : MonoBehaviour
+{
+    #region parameters
+    [SerializeField]
+    private Animator _myAnimator;
+    #endregion
+
+    #region references
+    private PlayerLifeComponent _myPlayerLife;
+    #endregion
+
+    #region methods
+    public void HealAni()
+    {
+        if (_myPlayerLife.Heal()) _myAnimator.SetTrigger("Life");
+        else _myAnimator.SetTrigger("Null");
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myPlayerLife = GetComponent<PlayerLifeComponent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
